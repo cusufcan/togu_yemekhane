caa36cc0c2d082e8bb76bd2ef7963a79f5c1090a
cusufcan <cusufcan@gmail.com>
2023-11-14 14:20:41 +0300

    scroll to index

--- lib/widget/yemekhane_widget.dart
+++ lib/widget/yemekhane_widget.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 
 import '../util/app_helper.dart';
 import 'date_text.dart';
 
-class YemekhaneWidget extends StatelessWidget {
+class YemekhaneWidget extends StatefulWidget {
   final List<List<String>>? data;
   final String? weekData;
 
@@ -14,71 +16,109 @@ class YemekhaneWidget extends StatelessWidget {
   });
 
   @override
+  State<YemekhaneWidget> createState() => _YemekhaneWidgetState();
+}
+
+class _YemekhaneWidgetState extends State<YemekhaneWidget> {
+  late AutoScrollController controller;
+  int weekDay = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AutoScrollController(
+      viewportBoundaryGetter: () => Rect.fromLTRB(0, 0, 0, MediaQuery.of(context).padding.bottom),
+      axis: Axis.vertical,
+    );
+  }
+
+  Future<void> _init() async {
+    weekDay = _getWeekDay();
+    await controller.scrollToIndex(weekDay != 0 ? weekDay - 1 : 0, preferPosition: AutoScrollPosition.begin);
+    controller.highlight(weekDay);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (data == null) {
+    if (widget.data == null) {
       return const Center(child: Text('N/A'));
     }
-    final int weekDay = _getWeekDay();
-    return Column(
-      children: [
-        const SizedBox(height: 20),
-        DateText(weekData.toString()),
-        Expanded(
-          child: ListView.builder(
-            physics: const BouncingScrollPhysics(decelerationRate: ScrollDecelerationRate.fast),
-            itemCount: data!.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding:
-                    EdgeInsets.fromLTRB(8, index == 0 || index == 4 ? 16 : 8, 8, index == 0 || index == 4 ? 16 : 8),
-                child: Row(
-                  children: [
-                    RotatedBox(
-                      quarterTurns: -1,
-                      child: Text(
-                        getDayName(index),
-                        style: Theme.of(context).textTheme.headlineSmall?.copyWith(),
-                      ),
-                    ),
-                    Expanded(
-                      child: Card(
-                        elevation: 3,
-                        color: weekDay != 0 && weekDay - 1 == index ? const Color.fromARGB(255, 184, 255, 184) : null,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(30),
-                            topLeft: Radius.circular(30),
-                            bottomRight: Radius.circular(300),
-                            topRight: Radius.circular(300),
+    return FutureBuilder(
+      future: _init(),
+      builder: (context, snapshot) {
+        return Column(
+          children: [
+            const SizedBox(height: 20),
+            DateText(widget.weekData.toString()),
+            Expanded(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                controller: controller,
+                physics: const BouncingScrollPhysics(decelerationRate: ScrollDecelerationRate.fast),
+                itemCount: widget.data!.length,
+                itemBuilder: (context, index) {
+                  return AutoScrollTag(
+                    index: index,
+                    controller: controller,
+                    key: ValueKey(index),
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(
+                          8, index == 0 || index == 4 ? 16 : 8, 8, index == 0 || index == 4 ? 16 : 8),
+                      child: Row(
+                        children: [
+                          RotatedBox(
+                            quarterTurns: -1,
+                            child: Text(
+                              getDayName(index),
+                              style: Theme.of(context).textTheme.headlineSmall?.copyWith(),
+                            ),
                           ),
-                        ),
-                        child: Column(
-                          children: data!
-                              .elementAt(index)
-                              .map(
-                                (e) => ListTile(
-                                  title: Text(
-                                    e.toString(),
-                                    style: TextStyle(
-                                      fontWeight:
-                                          data!.elementAt(index).elementAt(data!.elementAt(index).length - 1) == e
-                                              ? FontWeight.bold
-                                              : FontWeight.normal,
-                                    ),
-                                  ),
+                          Expanded(
+                            child: Card(
+                              elevation: 3,
+                              color: weekDay != 0 && weekDay - 1 == index
+                                  ? const Color.fromARGB(255, 184, 255, 184)
+                                  : null,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(30),
+                                  topLeft: Radius.circular(30),
+                                  bottomRight: Radius.circular(300),
+                                  topRight: Radius.circular(300),
                                 ),
-                              )
-                              .toList(),
-                        ),
+                              ),
+                              child: Column(
+                                children: widget.data!
+                                    .elementAt(index)
+                                    .map(
+                                      (e) => ListTile(
+                                        title: Text(
+                                          e.toString(),
+                                          style: TextStyle(
+                                            fontWeight: widget.data!
+                                                        .elementAt(index)
+                                                        .elementAt(widget.data!.elementAt(index).length - 1) ==
+                                                    e
+                                                ? FontWeight.bold
+                                                : FontWeight.normal,
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                    .toList(),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ),
-      ],
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 
